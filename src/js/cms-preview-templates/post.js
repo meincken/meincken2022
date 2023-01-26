4cfc70fd960e06764aa5538356c3f0272dba52dd
Nick Meincken <nick@meincken.com>
2023-01-26 09:52:47 +0000

    post fix

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -6,17 +6,17 @@ export default class PostPreview extends React.Component {
     const {entry, widgetFor, getAsset} = this.props;
     let image = getAsset(entry.getIn(["data", "image"]));
 
-    return <div className="mw6 center ph3 pv4">
-      <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
-      <div className="flex justify-between grey-3">
+    return <main>
+      <h1>{ entry.getIn(["data", "title"])}</h1>
+      <div>
         <p>{ format(entry.getIn(["data", "date"]), "iii, MMM d, yyyy") }</p>
         <p>Read in x minutes</p>
       </div>
-      <div className="cms mw6">
+      <div>
         <p>{ entry.getIn(["data", "description"]) }</p>
         { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
         { widgetFor("body") }
       </div>
-    </div>;
+    </main>;
   }
 }
